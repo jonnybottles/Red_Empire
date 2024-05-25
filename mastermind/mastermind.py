72efe76759c6b738e54626b7c889189b4b850634
Jonathan Butler <xxbutler86xx@gmail.com>
2024-05-25 16:20:12 -0400

    Remove uncessary comments.

--- mastermind/mastermind.py
+++ mastermind/mastermind.py
@@ -13,21 +13,6 @@ def main():
     if not os.path.exists("../data/listeners/"):
         os.mkdir("../data/listeners/")
 
-    # if os.path.exists("./data/databases/") == False:
-    # 	os.mkdir("./data/databases/")
-
-    # # Create Listener Object
-    # mylistener = Listener("Listener_4802", "", 9000)
-    # mylistener2 = Listener("Listener_4902", "", 1000)
-
-    # mylistener.app(mylistener.name, mylistener.ip_addr, mylistener.port)
-
-    # # Instantiate server object.
-    # http_server(mylistener)
-
-    # # Instantiate server object.
-    # http_server(mylistener2)
-
     home()
 
 
